c15f8cd0167a57e474de687c7a7cc23ded23202d
hk <kiptooharon.hk@gmail.com>
2024-06-30 23:23:19 +0300

    add entry.js script to updat cart info

--- web-static/js/entry.js
+++ web-static/js/entry.js
@@ -1,15 +1,78 @@
-const cart = {}
-
+//cart
+var cart = {}
+//add or minus menu
 let entry = document.createElement('div')
-
-entry.innerHTML = '<div class=minus>-</div><div class=count>0</div><div class=plus>+</div>'
 entry.className = "add_minus_cart"
+let minus = document.createElement('div')
+minus.className= 'minus'
+minus.innerHTML = '-'
+let count = document.createElement('div')
+count.className = 'count'
+count.innerHTML = '0'
+let plus = document.createElement('div')
+plus.className = 'plus'
+plus.innerHTML = '+'
+entry.appendChild(minus)
+entry.appendChild(count)
+entry.appendChild(plus)
+
+//items
 let items = document.querySelectorAll('.groceryItem')
+//add event listener to items
 
 for (let i = 0; i < items.length; i++){
-  let item = items[i];
-  let children = item.children
-  item.addEventListener("click", ()=> {
-    item.insertBefore(entry, children[1])});
+  items[i].addEventListener("click", ()=> {
+  if (items[i].children.length == 2) {
+      items[i].insertBefore(entry, items[i].children[1]);
+      count.innerHTML = '0';
+  }
+  if (items[i].id in cart) {
+      count.innerHTML = String(cart[items[i].id]);}
+  });
 }
-  
+
+plus.addEventListener('click', ()=>{
+  if (plus.parentElement.parentElement.id in cart) {
+   cart[plus.parentElement.parentElement.id] = cart[plus.parentElement.parentElement.id] + 1;
+   count.innerHTML = String(cart[plus.parentElement.parentElement.id]);
+  } else {
+   cart[plus.parentElement.parentElement.id] = 1;
+   count.innerHTML = String(cart[plus.parentElement.parentElement.id]);}
+  document.querySelector('#totalItems').innerHTML = String(Number(document.querySelector('#totalItems').innerHTML) + 1);
+});
+
+    
+minus.addEventListener('click', ()=>{
+ if (minus.parentElement.parentElement.id in cart) {
+   cart[minus.parentElement.parentElement.id] = cart[minus.parentElement.parentElement.id] - 1;
+   count.innerHTML = String(cart[minus.parentElement.parentElement.id]);
+   document.querySelector('#totalItems').innerHTML = String(Number(document.querySelector('#totalItems').innerHTML) - 1);
+  }});
+//bottom left entries
+
+let container = document.querySelector(".Xitems");
+
+
+plus.addEventListener('click', ()=> {
+  let __name = plus.parentElement.nextElementSibling.firstElementChild.firstElementChild.textContent;
+  let __price = plus.parentElement.nextElementSibling.lastElementChild.firstChild.textContent;
+  let _entry = document.querySelector(".XitemEntry").cloneNode();
+  _entry.appendChild(document.querySelector(".Xname").cloneNode());
+  _entry.appendChild(document.querySelector(".Xquantity").cloneNode());
+  _entry.appendChild(document.querySelector(".Xamount").cloneNode());
+  _entry.appendChild(document.querySelector(".Xremove").cloneNode());
+  _entry.children[0].textContent = __name;
+  _entry.children[1].textContent = __price;
+  _entry.children[2].textContent = Number(__price);
+  _entry.children[3].textContent = "X";
+  let _entries_ = document.querySelectorAll(".XitemEntry");
+  for (_entry_ of _entries_) {
+        if (_entry_.firstElementChild.textContent == __name) {
+         _entry_.children[2].textContent = String(Number(__price) * Number(cart[plus.parentElement.parentElement.id]));
+        document.querySelector("#totalcost").textContent = String(Number(document.querySelector("#totalcost").textContent) + Number(__price));
+        return;
+      }
+   }
+ container.insertBefore(_entry, container.children[0]);
+ document.querySelector("#totalcost").textContent = String(Number(document.querySelector("#totalcost").textContent) + Number(__price));
+}); 
--- web-static/js/submit.js
+++ web-static/js/submit.js
@@ -1,3 +1,12 @@
 document.querySelector("#completePurchase").addEventListener("click", ()=>{
- alert("order place");
+ if (document.querySelector("#totalcost").textContent == '0') {
+   alert("Empty Cart continue shopping")
+   document.querySelector(".Xcart").style.display = 'none';
+ } else {
+   document.querySelector('#pendingOrders').innerHTML = String(Number(document.querySelector('#pendingOrders').innerHTML) + 1);
+   document.querySelector('#totalItems').innerHTML = String(0);
+   document.querySelector(".Xcart").style.display = 'none';
+   cart = {}
+   alert("order placed");
+ }
 });
