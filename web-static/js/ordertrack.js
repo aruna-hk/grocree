155dd0020e31660f20b7bf2619887716db55381e
hk <kiptooharon.hk@gmail.com>
2024-07-10 20:16:24 +0300

    order tracking update--js to add order entry and status to ordertrack--to be updated b websocket '

---
+++ web-static/js/ordertrack.js
@@ -0,0 +1,6 @@
+document.querySelector("#ordertrack > #orders").addEventListener('click', ()=> {
+ document.querySelector(".Track").style.visibility = "visible";
+});
+document.querySelector(".Track button").addEventListener('click', ()=> {
+ document.querySelector('.Track').style.visibility = 'hidden';
+});
--- web-static/js/submit.js
+++ web-static/js/submit.js
@@ -6,12 +6,30 @@ const action = "orders"
 async function order() {
  let userURL = orderApi + document.querySelector("#USER>a>img").id + "/" + action
  let orderResponse = await fetch(userURL, {"headers":headers, "body":JSON.stringify(cart), "method":"POST"})
- if (orderResponse.status == 201) {alert(await orderResponse.text())}
+ if (orderResponse.status == 201) {
+  let __orderInfo = await orderResponse.json()
+  let __entry = document.querySelector(".Trackentries > .orderentry").cloneNode()
+  __entry.appendChild(document.createElement("div"))
+  __entry.appendChild(document.createElement("div"))
+  __entry.appendChild(document.createElement("div"))
+  __entry.firstElementChild.textContent = __orderInfo['Time']
+  __entry.lastElementChild.textContent = __orderInfo['status']
+  __entry.children[1].textContent = __orderInfo['DeliveryPerson']
+
+  if (document.querySelector(".Track > .Trackentries").firstElementChild.firstElementChild.textContent == "-") {
+   document.querySelector(".Track > .Trackentries").removeChild(document.querySelector(".Track > .Trackentries").firstElementChild)
+   }
+  document.querySelector(".Track > .Trackentries").append(__entry)
+  alert("Order Success")
+ }
 }
 document.querySelector("#completePurchase").addEventListener("click", ()=>{
  if (document.querySelector("#totalcost").textContent == '0') {
    alert("Empty Cart continue shopping")
    document.querySelector(".Xcart").style.display = 'none';
+ } else if (document.querySelector("#USER>#username").textContent == "login/register") {
+   alert("Login to complete Order")
+   document.querySelector("#USER>#username").click()
  } else {
    document.querySelector('#pendingOrders').innerHTML = String(Number(document.querySelector('#pendingOrders').innerHTML) + 1);
    document.querySelector('#totalItems').innerHTML = String(0);
